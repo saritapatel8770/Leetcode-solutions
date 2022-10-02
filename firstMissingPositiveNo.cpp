fc28d4200d1b5e2f4faa6f22b441428b3f43d343
harmanjit356 <114508310+harmanjit356@users.noreply.github.com>
2022-10-02 23:44:58 +0530

    Create firstMissingPositiveNo.cpp
    First Missing Positive Number in cpp

---
+++ firstMissingPositiveNo.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> arr(n+1,0);
+        for(int i=0;i<n;i++)if(nums[i]>0 && nums[i]<=n)arr[nums[i]]=1;
+        for(int i=1;i<=n;i++){
+            if(arr[i]==0)return i;
+        }
+        return n+1;
+    }
+};
