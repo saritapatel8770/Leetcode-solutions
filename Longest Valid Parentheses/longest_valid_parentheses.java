ca81edcd8a13e32ee27b11d21095a1511742cfad
Kishansharma06 <Kishansharma060514@gmail.com>
2022-10-04 03:00:40 +0530

    Longest Substring Without Repeating Characters  solution in c++

--- Longest Valid Parentheses/longest_valid_parentheses.java
+++ Longest Valid Parentheses/longest_valid_parentheses.java
@@ -1,31 +1,22 @@
-public class Solution {
-    public int longestValidParentheses(String s) {
-        int left = 0, right = 0, maxlength = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                left++;
-            } else {
-                right++;
-            }
-            if (left == right) {
-                maxlength = Math.max(maxlength, 2 * right);
-            } else if (right >= left) {
-                left = right = 0;
-            }
-        }
-        left = right = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == '(') {
-                left++;
-            } else {
-                right++;
-            }
-            if (left == right) {
-                maxlength = Math.max(maxlength, 2 * left);
-            } else if (left >= right) {
-                left = right = 0;
-            }
-        }
-        return maxlength;
+// Runtime: 1 ms.
+// Memory Usage: 41.8 MB.
+
+class Solution {
+  private int helper(String s, int start, int end, int shift, char sign){
+    int n = 0, maxi = 0, tmp = 0;
+    
+    for(int i = start; i != end; i += shift){
+        tmp++;
+        n += s.charAt(i) == sign ? -1 : 1;
+          
+        if( n == 0) maxi = Math.max(maxi, tmp);
+        else if( n < 0 ) n = tmp = 0;
+      }
+    
+      return maxi;
     }
+  
+  public int longestValidParentheses(String s) {
+    return Math.max(helper(s, 0, s.length(), 1, ')'), helper(s, s.length() - 1, -1, -1, '('));    
+  }
 }
