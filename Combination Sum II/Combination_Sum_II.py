e26e917088282a4ddb822426dae4aae24a8ffb73
Vatsal Dalal <67739998+vatsal9117@users.noreply.github.com>
2022-10-04 10:45:29 +0530

    Update Combination_Sum_II.py

--- Combination Sum II/Combination_Sum_II.py
+++ Combination Sum II/Combination_Sum_II.py
@@ -1,8 +1,20 @@
 class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    numToIndex = {}
+    def combinationSum2(self, nums, target):
+            def bt(tlist, start, target):
+                s = sum(tlist)
+                if s == target:
+                    res.append(tlist[::])
+                    return
+                if s > target:
+                    return
 
-    for i, num in enumerate(nums):
-      if target - num in numToIndex:
-        return numToIndex[target - num], i
-      numToIndex[num] = i
+                for i in range(start, len(nums)):
+                    if i > start and nums[i] == nums[i - 1]:
+                        continue
+                    tlist.append(nums[i])
+                    bt(tlist, i + 1, target)
+                    tlist.pop()
+            nums.sort()
+            res = []
+            bt([], 0, target)
+            return res
