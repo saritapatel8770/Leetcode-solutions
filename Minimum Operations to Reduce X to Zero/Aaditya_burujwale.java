b879d74e94a87eacff8f4d7a9747af3744176fde
Aaditya Burujwale <63345229+Aadityapburujwale@users.noreply.github.com>
2022-10-14 16:14:05 +0530

    Create Aaditya_burujwale.java

---
+++ Minimum Operations to Reduce X to Zero/Aaditya_burujwale.java
@@ -0,0 +1,35 @@
+public int minOperations(int[] nums, int x) {
+        
+        
+      int target = -1 * x;
+        for(int e: nums){
+            target += e;
+        }
+        
+        if(target == 0){
+            return nums.length;
+        }
+        if(target < 0){
+            return -1;
+        }
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int m = -1;
+        
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(sum == target){
+                m = i + 1;
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
+            }
+            if(map.containsKey(sum - target)){
+                if(m < (i - map.get(sum - target))){
+                    m = i - map.get(sum - target);
+                }
+            }
+        }
+        return m != -1 ? nums.length - m : m;
+    }
